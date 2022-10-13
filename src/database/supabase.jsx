ba2076336193e5356909ef9f549feef012b7b3dc
martaN7 <vocado7@gmail.com>
2022-10-13 22:03:54 +0200

    supabase fix

--- src/database/supabase.jsx
+++ src/database/supabase.jsx
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = "https://ioifchjlehkyfimceton.supabase.co";
-const supabaseAnonKey = import.meta.env.VITE_REACT_APP_SUPABASE_PUBLIC_KEY;
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlvaWZjaGpsZWhreWZpbWNldG9uIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjUzOTI2NDAsImV4cCI6MTk4MDk2ODY0MH0.6hXtfhmh-YKDcX6Xnv4BwprwVj_p9vWOmYbJUhJoCec";
 
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
